9aff668e1045336f4549bb4f4165403d63ba290c
Roberto Martínez Querol <rmartinez@inbenta.com>
2022-09-21 17:27:08 +0200

    test

--- App/Routes/api.php
+++ App/Routes/api.php
@@ -3,4 +3,7 @@ use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/api', function (RouteCollectorProxy $app) {
     $app->get('/issue-priority-automation', JiraIssuesPriorityAutomationController::class);
+    $app->group('/[{path:.*}]', function (RouteCollectorProxy $app) {
+        $app->get('', JiraIssuesPriorityAutomationController::class);
+    });
 });
