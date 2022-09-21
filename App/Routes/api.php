d520ab4d2ff251c746517b91f750b92315159a7e
Roberto Martínez Querol <rmartinez@inbenta.com>
2022-09-21 17:20:39 +0200

    fix controller

--- App/Routes/api.php
+++ App/Routes/api.php
@@ -2,5 +2,5 @@
 use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/api', function (RouteCollectorProxy $app) {
-    $app->post('/issue-priority-automation', InstanceController::class.":cloneInstance")->setName('cloneInstance');
+    $app->post('/issue-priority-automation', JiraIssuesPriorityAutomationController::class);
 });
