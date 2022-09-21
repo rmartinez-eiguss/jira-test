2f04d87c54e50a9fe54e4fee953ab38bc6fd192f
Roberto Martínez Querol <rmartinez@inbenta.com>
2022-09-21 17:44:53 +0200

    add procfile

--- App/Routes/api.php
+++ App/Routes/api.php
@@ -2,4 +2,5 @@
 use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/api', function (RouteCollectorProxy $app) {
+    $app->get('/issue-priority-automation', JiraIssuesPriorityAutomationController::class);
 });
