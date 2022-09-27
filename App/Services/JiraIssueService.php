708d211c224ff3d9c3f6ae99a7d395b557669516
Roberto Martínez Querol <rmartinez@inbenta.com>
2022-09-27 13:02:25 +0200

    add ACV management

--- App/Services/JiraIssueService.php
+++ App/Services/JiraIssueService.php
@@ -15,21 +15,29 @@ class JiraIssueService
         'Story Points',
     ];
     protected $storyPointsFieldId = "customfield_10117";
+    protected $requestedByCustomerFieldId = "customfield_10801";
+    protected $requestedByCustomerScore = [
+        0       => 1,
+        50000   => 25,
+        150000  => 50,
+        300000  => 75,
+        500000  => 100,
+    ];
     protected $productDirectionAlignmentFieldId = "customfield_11409";
     protected $productDirectionAlignmentsScore = [
-        'Totally aligned' => 5,
-        'Quite aligned' => 4,
-        'Partially aligned' => 3,
-        'Not aligned' => 2,
         'Against the direction' => 1,
+        'Not aligned' => 2,
+        'Partially aligned' => 3,
+        'Quite aligned' => 4,
+        'Totally aligned' => 5,
     ];
     protected $severityFieldId = "customfield_11103";
     protected $severitiesScore = [
-        "Meteor" => 100,
-        "Major" => 75,
-        "Moderate" => 50,
-        "Minor" => 25,
         "Cosmetic" => 1,
+        "Minor" => 25,
+        "Moderate" => 50,
+        "Major" => 75,
+        "Meteor" => 100,
     ];
     protected $priorityScoreFieldId = "customfield_11408";
 
@@ -65,6 +73,7 @@ class JiraIssueService
                         $this->severityFieldId,
                         $this->storyPointsFieldId,
                         $this->productDirectionAlignmentFieldId,
+                        $this->requestedByCustomerFieldId,
                     ],
                 )
             ];
@@ -136,9 +145,33 @@ class JiraIssueService
      */
     protected function getCustomerScore($issue)
     {
-        // TODO call grandpa
 
-        return 50;
+        $customers = $issue->fields->{$this->requestedByCustomerFieldId};
+        $customersACV = 0;
+        foreach ($customers as $customer) {
+            $customersACV += $this->getCustomerACV($customer);
+        }
+
+        $requestedByCustomerScore = 0;
+        foreach ($this->requestedByCustomerScore as $acv => $score) {
+            if($customersACV>$acv){
+                $requestedByCustomerScore = $score;
+            }
+        }
+
+        return $requestedByCustomerScore;
+    }
+
+    /**
+     * Return the customer ACV in grandpa
+     *
+     * @param string $customer
+     * @return integer
+     */
+    protected function getCustomerACV($customer)
+    {
+        // TODO call grandpa
+        return 100000;
     }
 
     /**
